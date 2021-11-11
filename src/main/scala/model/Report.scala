e43265599550cd3ec9e35d66903ac0f2b1e9d269
Yakouttkadiri <57764694+Yakouttkadiri@users.noreply.github.com>
2021-11-11 22:04:30 +0100

    Update Report.scala

--- src/main/scala/model/Report.scala
+++ src/main/scala/model/Report.scala
@@ -1,11 +1,17 @@
-package model
+import controller.CSV
+import model.{Airport, Country, Runway}
 import model.Airport.countryFromCode
-import model.Query.{airports, deriveCountryCode, runways}
-
 import scala.collection.MapView
-import scala.collection.immutable.Nil.groupBy
 import scala.util.Try
-object Report{
+object Report {
+  
+  /** **************************** Parse CSV : countries, airports and runways associated as Iterators of case classes ********************* */
+  val airports: Iterator[Airport] = CSV.read("airports.csv", Airport.parseAirport).lines
+  val countries: Iterator[Country] = CSV.read("countries.csv", Country.parseCountry).lines
+  val runways: Iterator[Runway] = CSV.read("runways.csv", Runway.parseRunway).lines
+  /** ******************************************************************************************************* */
+  
+
   def getInput() = {
     Console.println("***********Report****************" +
       "\" \" +\n      \"/*************** Choice / Menu : ************************************\\n     \" +" +
@@ -14,85 +20,72 @@ object Report{
       "* 3)    top 10 most common runway latitude (\\\"le_ident\\\")\\n     \" +\n      " +
       "\"********************************************************************/\"")
     val option_rep = Console.in.read()
-    println("Vous avez choisi, "+ option_rep)
+    println("Vous avez choisi, " + option_rep)
     option_rep
-
   }
+  
+  
   def InitReport(option_rep: Int): Unit = option_rep match {
-    case 1 => HighestAirports()
-    case 2 => LowestAirports()
+    case 1 => //HighestAirports()
+    case 2 => //LowestAirports()
     case 3 => TypeRunway()
     case 4 => Top10Identity()
   }
-
+  
+  
   /**
    * A List with Tuple2 of country name and the count of airports.
    */
-  val groupByMapAirportsCount: List[(String, Int)] = airports.map(x => x.iso_country).toList.groupBy(identity).mapValues { airports.size }
-
-  /***
-   * List of 10 countries with highest number of airports
-   *
-   * MapReduce =>
-   *
-   * 1) Map : retrieves tuples from countries (iso_country) and from airports (code)
-   * => compare the two and increment the number of airports => we get at the end a list
-   * of tuples containing the (name of the country, number of airports)
-   *
-   * 2) Reduce : we sort the list in descending order to keep the first first tuples and return them
-   *
-   *
-   */
-  def HighestAirports(): List[Airport] ={
+  //Iterator[Airport] != List[Airport] ;
+  /*val groupByMapAirportsCount: List[(String, Int)] = airports.map(x => x.iso_country).toList.groupBy(identity).mapValues { airports.length }
+  def HighestAirports(): List[(String, Int)] ={
+    /*** List of 10 countries with highest number of airports
+     * MapReduce => * 1) Map : retrieves tuples from countries (iso_country) and from airports (code)
+                    * => compare the two and increment the number of airports => we get at the end a list
+                    * of tuples containing the (name of the country, number of airports)
+                    * 2) Reduce : we sort the list in descending order to keep the first first tuples and return them */
     val reverseSortedCountries: List[(String, Int)] = groupByMapAirportsCount.sortWith(-_._2 < -_._2)
-    val topTenAirportCountries: List[(String, Int)] = reverseSortedCountries.take(10).map { case (code, count) => (countryFromCode(code), count) }
+    reverseSortedCountries.take(10).map { case (code, count) => (countryFromCode(code), count) }
   }
 
-  /***
-   * List of 10 countries with lowest number of airports
-   *
-   * MapReduce =>
-   *
-   * 1) Map : retrieves tuples from countries (iso_country) and from airports (code)
-   * => compare the two and increment the number of airports => we get at the end a list
-   * of tuples containing the (name of the country, number of airports)
-   *
-   * 2) Reduce : we sort the list in ascending order to keep the first first tuples and return them
-   *
-   *
-   */
   def LowestAirports(): List[(String, Int)] ={
+    /*** List of 10 countries with lowest number of airports ***/
     val sortedCountries: List[(String, Int)] = groupByMapAirportsCount.sortWith(_._2 < _._2)
-    sortedCountries
-  }
-
-  /******
-   * Type of Runways ("surface") per Country
-   */
-  def TypeRunway(): MapView[String,Set[String]] ={
+    sortedCountries.take(10).map { case (code, count) => (countryFromCode(code), count) }
+  }*/
+  
+  
+  def TypeRunway(): MapView[String, Set[String]] = {
+    /** * Type of Runways ("surface") per Country ** */
     val SurfaceType: Map[String, String] = runways.map { x => (x.airport_ident, x.surface) }.toMap
-    val listCountryType: List[(String, String)] = airports.map { x => (countryFromCode(x.iso_country),Try { SurfaceType(x.ident) }.getOrElse(""))}.toList
-    /***** grouping the above list structure to obtain a country with its set of runways.
+    val listCountryType: List[(String, String)] = airports.map { x => (countryFromCode(x.iso_country), Try {
+      SurfaceType(x.ident)
+    }.getOrElse(""))
+    }.toList
+    /** *** Grouping the above list structure to obtain a country with its set of runways
      * Also filtering out countries for which surface information is not available.
      */
-    val runwaysSurfacePerCountry: MapView[String, Set[String]] = listCountryType groupBy (_._1) mapValues
-      { x => x.filterNot { _._2 == "" }.map(_._2).toSet }
+    
+    
+    val runwaysSurfacePerCountry: MapView[String, Set[String]] = listCountryType groupBy (_._1) mapValues { x => x.filterNot {
+      _._2 == ""
+    }.map(_._2).toSet
+    }
     runwaysSurfacePerCountry
   }
-    /**
-     * grouping the above list structure to obtain a country with its set of runways.
-     * Also filtering out countries for which surface information is not available.
-     */
-  /******
-   * The 10 most common runway identity ("le_ident")
-   */
-  def Top10Identity(): List[String] ={
+  
+  
+  def Top10Identity(): List[String] = {
+    /** ** The 10 most common runway identity ("le_ident") *** */
     // runways : table
-    // select * from runways
+    // select * from runwys
     //  on veut common identity => select identity from runways group by
-    val mostCommonRunways: List[String] = runways.map(x => Try { x.le_ident }.getOrElse("")).toList.groupBy(identity)
-      .mapValues { _.size }.toList.sortWith(-_._2 < -_._2).take(10).map { case (id, count) => id }
+    val mostCommonRunways: List[String] = runways.map(x => Try {
+      x.le_ident
+    }.getOrElse("")).toList.groupBy(identity)
+      .mapValues {
+        _.size
+      }.toList.sortWith(-_._2 < -_._2).take(10).map { case (id, count) => id }
     mostCommonRunways
-    //val mostCommonRunways: List[String] = runways.map(x => x.airport_ident)}.getOrElse("").toList.groupBy(identity).mapValues { runways.size }.toList.sortWith(-_._2 < -_._2).take(10).map { case (id, count) => id }
   }
 }
