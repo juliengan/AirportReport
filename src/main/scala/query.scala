c3d17a33f82b2d1e66ac1c798ef9bb31b4893275
Yakouttkadiri <57764694+Yakouttkadiri@users.noreply.github.com>
2021-11-11 11:09:27 +0100

    Update query.scala

--- src/main/scala/query.scala
+++ src/main/scala/query.scala
@@ -1,1 +1,18 @@
+package scala
 
+import scala.util.Try
+
+
+object Query { 
+  
+  
+Console.println("Vous avez choisi #1 Query !")
+
+// Entrée utilisateur code ou nom de Pays
+  
+ 
+  
+val Pays = scala.io.StdIn.readLine("Veuillez choisir un code ou nom de Pays ")
+
+ 
+}
