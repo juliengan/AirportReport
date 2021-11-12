acee2b196e1bece8cd06fcce5c02aebeae9dbc1f
juliengan <38871210+juliengan@users.noreply.github.com>
2021-11-12 22:36:12 +0100

    rectified Tests

--- src/test/scala/ScalaTesting.scala
+++ src/test/scala/ScalaTesting.scala
@@ -20,57 +20,61 @@ class ScalaTesting extends Matchers {
 }
 
 class ScalaTesting2 extends AnyFunSuite{
-  test("InitReportTest"){
-    assert(Report.InitReport() == "test checked ")
+  test("ReportTest"){
+    assert(Report.commonIdentity() == "commonIdentity() checked")
+    assert(Report.runwayTypes() == "runwayTypes() checked")
+    assert(Report.highest() == "highest() checked")
+    assert(Report.lowest() == "lowest() checked")
+
   }
 
   test("QueryTest"){
-    assert(Main.getAirportsWithRunways("VA").contains(AirportAndRunways("VA", "Vatican City Heliport", "VA-0001", List())))
+    assert(Main.getAirportsWithRunways("VA").contains(AirportAndRunways("", "Vatican City Heliport", "VA-0001", List())))
   }
 
 
   test("parseCSV as List of String Test"){
-    val airportsData = parseAirport()
-    assert(airportsData.map(_.split(",")(0)).head(0).toInt == 6523)
-    assert(airportsData.map(_.split(",")(1)).head(0).toString == "00A")
-    assert(airportsData.map(_.split(",")(2)).head(0).toString == "heliport")
-    assert(airportsData.map(_.split(",")(3)).head(0).toString == "Total Rf Heliport")
-    assert(airportsData.map(_.split(",")(4)).head(0) == 40.07080078125)
-    assert(airportsData.map(_.split(",")(5)).head(0) == -74.93360137939453)
-    assert(airportsData.map(_.split(",")(6)).head(0) == 11)
-    assert(airportsData.map(_.split(",")(7)).head(0).toString == "NA")
-    assert(airportsData.map(_.split(",")(8)).head(0).toString == "US")
-    assert(airportsData.map(_.split(",")(9)).head(0).toString == "US-PA")
-    assert(airportsData.map(_.split(",")(10)).head(0).toString == "Bensalem")
-    assert(airportsData.map(_.split(",")(11)).head(0).toString == "no")
-    assert(airportsData.map(_.split(",")(12)).head(0).toString == "00A")
-    assert(airportsData.map(_.split(",")(13)).head(0).toString == "")
-    assert(airportsData.map(_.split(",")(14)).head(0).toString == "00A")
-    assert(airportsData.map(_.split(",")(15)).head(0).toString == "")
-    assert(airportsData.map(_.split(",")(16)).head(0).toString == "")
-    assert(airportsData.map(_.split(",")(17)).head(0).toString == "")
+    val airportsData = parseAirport().head
+    assert(airportsData.split(",")(0).toInt == 6523)
+    assert(airportsData.split(",")(1).equals("\"00A\""))
+    assert(airportsData.split(",")(2).equals("\"heliport\""))
+    assert(airportsData.split(",")(3)equals("\"Total Rf Heliport\""))
+    assert(airportsData.split(",")(4).toFloat == 40.07080078125)
+    assert(airportsData.split(",")(5).toFloat == -74.93360137939453)
+    assert(airportsData.split(",")(6).toInt == 11)
+    assert(airportsData.split(",")(7).equals("\"NA\""))
+    assert(airportsData.split(",")(8).equals("\"US\""))
+    assert(airportsData.split(",")(9).equals("\"US-PA\""))
+    assert(airportsData.split(",")(10).equals("\"Bensalem\""))
+    assert(airportsData.split(",")(11).equals("\"no\""))
+    assert(airportsData.split(",")(12).equals("\"00A\""))
+    assert(airportsData.split(",")(13).equals(""))
+    assert(airportsData.split(",")(14).equals("\"00A\""))
+    assert(airportsData.split(",")(15).equals(""))
+    assert(airportsData.split(",")(16).equals(""))
+    assert(airportsData.split(",")(17).equals(""))
   }
 
   test("parseCSV as List of case class (Airport, Country, Runway)"){
-    val airports: List[Airport] = CSV.read("airports.csv", Airport.parseAirport).lines.toList
-    assert(airports.map(x=>(x.id)).head(0).toInt == 6523)
-    assert(airports.map(x=>(x.ident)).head(0).toString == "00A")
-    assert(airports.map(x=>(x.airport_type)).head(0).toString == "heliport")
-    assert(airports.map(x=>(x.name)).head(0).toString == "Total Rf Heliport")
-    assert(airports.map(x=>(x.lat_deg)).head(0) == 40.07080078125)
-    assert(airports.map(x=>(x.lon_deg)).head(0) == -74.93360137939453)
-    assert(airports.map(x=>(x.elevation_dt)).head(0) == 11)
-    assert(airports.map(x=>(x.continent)).head(0).toString == "NA")
-    assert(airports.map(x=>(x.iso_country)).head(0).toString == "US")
-    assert(airports.map(x=>(x.iso_region)).head(0).toString == "US-PA")
-    assert(airports.map(x=>(x.municipality)).head(0).toString == "Bensalem")
-    assert(airports.map(x=>(x.scheduled_service)).head(0).toString == "no")
-    assert(airports.map(x=>(x.gps_code)).head(0).toString == "00A")
-    assert(airports.map(x=>(x.iata_code)).head(0) == 6523)
-    assert(airports.map(x=>(x.local_code)).head(0) == 6523)
-    assert(airports.map(x=>(x.home_link)).head(0) == 6523)
-    assert(airports.map(x=>(x.wikipedia_link)).head(0) == 6523)
-    assert(airports.map(x=>(x.keywords)).head(0) == 6523)
+    val airport: Airport = CSV.read("airports.csv", Airport.parseAirport).lines.next()
+    assert(airport.id.toInt == 6530)
+    assert(airport.ident.equals("\"00A\""))
+    assert(airport.airport_type.equals("\"heliport\""))
+    assert(airport.name.equals("\"Total Rf Heliport\""))
+    assert(airport.lat_deg.toFloat == 40.07080078125)
+    assert(airport.lon_deg.toFloat == -74.93360137939453)
+    assert(airport.elevation_dt.toInt == 11)
+    assert(airport.continent.equals("\"NA\""))
+    assert(airport.iso_country.equals("\"US\""))
+    assert(airport.iso_region.equals("\"US-PA\""))
+    assert(airport.municipality.equals("\"Bensalem\""))
+    assert(airport.scheduled_service.equals("\"no\""))
+    assert(airport.gps_code.equals("\"00A\""))
+    assert(airport.iata_code.equals(""))
+    assert(airport.local_code.equals("\"00A\""))
+    assert(airport.home_link.equals(""))
+    assert(airport.wikipedia_link.equals(""))
+    assert(airport.keywords.equals(""))
   }
 
 }
