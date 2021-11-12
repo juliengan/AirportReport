e6c48acba82bc48b21c2d789fec1ef57f74d7620
juliengan <38871210+juliengan@users.noreply.github.com>
2021-11-12 21:18:38 +0100

    update country.scala

--- src/main/scala/model/Country.scala
+++ src/main/scala/model/Country.scala
@@ -2,16 +2,23 @@ package model
 
 import scala.util.Try
 
-case class Country(id: String, code: String, name: String, continent: String, wikipedia_link: String, keywords: String)
+case class Country(
+                    id: String,
+                    code: String,
+                    name: String,
+                    continent: String,
+                    wikipedia_link: String,
+                    keywords: String)
 
+// Companion object
 object Country {
   //pattern matching is forbose so you can implement detailed error msg if you feel like it
   def parseCountry(line: Array[String]):Option[Country] = {
     (Try(line(0)).toOption, Try(line(1).toString).toOption,Try(line(2).toString).toOption, Try(line(3).toString).toOption,
       Try(line(4)).toOption, Try(line(5).toString).toOption)
+
     match {
-      case (Some(a), Some(b), Some(c), Some(d), Some(e), Some(f))
-      => Some(Country(a,b,c,d,e,f))
+      case (Some(a), Some(b), Some(c), Some(d), Some(e), Some(f)) => Some(Country(a,b,c,d,e,f))
       case _ => None
     }
   }
