c8fd22b6b6bc6a21a16d526d5c5b6af0caf12032
juliengan <38871210+juliengan@users.noreply.github.com>
2021-11-12 23:31:11 +0100

    Update Report.scala

--- src/main/scala/Report.scala
+++ src/main/scala/Report.scala
@@ -6,7 +6,7 @@ object Report {
   val countriesData = parseCountry()
   val airportsData = parseAirport()
   val runwaysData = parseRunway()
-  def highest() = {
+  def highest(): String = {
     val AirportsCount: List[(String, Int)] = airportsData.map(_.split(",")(8)).groupBy(identity).mapValues { _.size }.toList
     /*** List of 10 countries with highest number of airports
      * MapReduce => * 1) Map : retrieves tuples from countries (iso_country) and from airports (code)
@@ -16,27 +16,29 @@ object Report {
     val reverseSortedCountries: List[(String, Int)] = AirportsCount.sortWith(-_._2 < -_._2)
     val topTenAirportsCountries: List[(String, Int)] = reverseSortedCountries.take(10)
     topTenAirportsCountries.foreach(x=>println(x._1,x._2))
-    "test checked "
+    "highest() checked"
   }
 
-  def lowest() ={
+  def lowest(): String ={
     /*** List of 10 countries with lowest number of airports ***/
     val AirportsCount: List[(String, Int)] = airportsData.map(_.split(",")(8)).groupBy(identity).mapValues { _.size }.toList
     val sortedCountries: List[(String, Int)] = AirportsCount.sortWith(_._2 < _._2)
     val bottomTenAirportsCountries: List[(String, Int)] = sortedCountries.take(10)
     bottomTenAirportsCountries.foreach(x=>println(x._1,x._2))
-    "test checked "
+    "lowest() checked"
   }
 
-  def runwayTypes() = {
+  def runwayTypes(): String = {
     /** * Type of Runways ("surface") per Country ** */
     val SurfaceType = runways.map { x => (x.airport_ident, x.surface) }.toMap
     val listCountryType = airports.map(x => (countryFromCode(x.iso_country), Try {SurfaceType(x.ident)}.getOrElse(""))).toList
     val runwaysSurfacePerCountry = listCountryType groupBy (_._1) mapValues { x => x.filterNot {_._2 == ""}.map(_._2)}
     runwaysSurfacePerCountry.toList.foreach(x=>println(x))
+    "runwayTypes() checked"
+
   }
 
-  def commonIdentity() = {
+  def commonIdentity(): String = {
     /** ** The 10 most common runway identity ("le_ident") *** */
     // runways : table
     // select * from runwys
@@ -44,6 +46,8 @@ object Report {
     val mostCommonRunways: List[String] = runways.map(x => Try {x.le_ident}.getOrElse("")).toList.groupBy(identity).mapValues {_.size
     }.toList.sortWith(-_._2 < -_._2).take(10).map { case (id, count) => id }
     mostCommonRunways.foreach(x=>println(x))
+    "commonIdentity() checked"
+
   }
 
   def InitReport(): Any = scala.io.StdIn.readLine("Please enter your choice > ") match {
@@ -58,4 +62,4 @@ object Report {
     case _ => println("Wrong input : chose between 1 and 4")
   }
 
-} 
+}
